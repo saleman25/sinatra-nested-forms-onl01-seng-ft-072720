a7bfd7afeea889a3954ee71f8f10dd4821383fc7
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-09 03:18:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,10 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
 
 get '/' do
 erb :root
 end 
-
-# get '/new'
-# erb :new 
-# end 
 
 
 
