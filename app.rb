26bebcf6998712cd187af0687527f8a069599134
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-09 03:25:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,15 @@ get '/new' do
 erb :'pirates/new'
 end 
 
+post '/pirates' do
+  @pirate = Pirate.new(params[:pirate])
 
+  params[:pirate][:ships].each do |details|
+        Ship.new(details)
+  end
+  @ships = Ship.all
 
+  erb :'pirates/show'
+  end
+end
 end 
