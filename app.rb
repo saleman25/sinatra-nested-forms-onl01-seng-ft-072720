06c7f06a3066437048ff555b5a699395045bb3a7
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-09 03:26:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,23 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
 
-get '/' do
-erb :root
-end 
+ get '/' do
+      erb :root
+    end
 
-get '/new' do
-erb :'pirates/new'
-end 
+    get '/new' do
+      erb :'pirates/new'
+    end
 
-post '/pirates' do
-  @pirate = Pirate.new(params[:pirate])
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
-  params[:pirate][:ships].each do |details|
+      params[:pirate][:ships].each do |details|
         Ship.new(details)
-  end
-  @ships = Ship.all
+      end
+      @ships = Ship.all
 
-  erb :'pirates/show'
+      erb :'pirates/show'
+    end
+    
   end
 end
-end 
